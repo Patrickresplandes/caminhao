08c72c5ae05f1df4ffcdc4177db5d11cfdca7455
Patrickresplandes <patrickresplandes87@gmail.com>
2024-07-30 12:29:46 -0300

    fix: troca de link

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -57,7 +57,7 @@ export default function Login() {
                 </div>
                 <button type="submit" className="w-full text-white bg-indigo-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                 <p className="text-sm font-light text-indigo-500 dark:text-indigo-400">
-                  Don’t have an account yet? <Link href="/Registro" className="font-medium text-indigo-600 hover:underline dark:text-primary-500">Sign up</Link>
+                  Don’t have an account yet? <Link href="/registro" className="font-medium text-indigo-600 hover:underline dark:text-primary-500">Sign up</Link>
                 </p>
               </form>
             </div>
